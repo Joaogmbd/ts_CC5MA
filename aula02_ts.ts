c1d7be241e992ad9e1d6cf0f183c6bc09c3ce576
João Gabriel Moreira Bossatto Daré <79703333+Joaogmbd@users.noreply.github.com>
2023-02-09 08:51:42 -0300

    Update aula02_ts.ts
    comentarios

--- aula02_ts.ts
+++ aula02_ts.ts
@@ -1,9 +1,10 @@
+//Cria interface pessoa
 interface IPessoaEx {
 	nome: string;
 	sobrenome: string;
 	validarDados: (x: string) => boolean;
 }
-
+//Classe Contacorrente
 class ContaCorrenteEx {
 	correntista: IPessoaEx;
 	saldo: number;
@@ -19,6 +20,7 @@ class ContaCorrenteEx {
 		return "Nome completo: " + this.correntista.nome + " " + this.correntista.sobrenome; 
 	}
 }
+//Classe InterfacePessoaFisica , sobrescrevendo atributos da interface IpessoaEx
 class IPessoaFisicaEx implements IPessoaEx {
 	nome: string;
 	sobrenome: string;
@@ -41,16 +43,9 @@ class IPessoaFisicaEx implements IPessoaEx {
 
 }
 
-
+//definindo correntista1 do tipo pessoaFisica.
 let correntista1 = new IPessoaFisicaEx("Carlos", "da Silva", "123.123-12")
-/*
-let correntista1: IPessoaFisicaEx = {
-	nome: "Carlos",
-	sobrenome: "da Silva",
-	cpf: "123.123-12"
-};
-*/
-
+//criando objeto do tipo contaCorrente, passando o correntista do tipo pessoa fisica e seu saldo
 let obj1 = new ContaCorrenteEx(correntista1, 1000); 
 console.log(obj1.imprimirDadosPf())
 console.log(obj1.correntista.validarDados(correntista1.cpf));
